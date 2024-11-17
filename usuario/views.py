d947ead15876b8f487a1c52fc9009e0c3b447422
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-11-16 23:37:23 -0500

    cambios, vista para insertar un usuario en la tabla USUARIO y cambios en el template registros

--- usuario/views.py
+++ usuario/views.py
@@ -5,7 +5,38 @@ from django.db import connection
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
-def hola(request):
-    return render(request, 'hola.html')
+# vista para insertar un usuario en la tabla USUARIO
+def registro(request):
+    if request.method == 'POST':
+        try:
+
+            documento = request.POST['documento']
+            nombre = request.POST['nombre']
+            apellido = request.POST['apellido']
+            correo = request.POST['correo']
+            password = request.POST['password']
+            fecha_nac = request.POST['fecha_nac']
+            ubicacion = request.POST['ubicacion']
+
+            #cifrar el password
+            hashed_password = make_password(password)
+
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                            INSERT INTO USUARIO (id_us, nombre, apellido, correo, password, fecha_nac, ubicacion)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        """, [documento, nombre, apellido, correo, hashed_password, fecha_nac, ubicacion])
+        
+            messages.success(request, "Registro de usuario exitoso.")
+            return redirect('registro')
+    
+        except Exception as e:
+            error_message = f"Error en el registro de usuario: {str(e)}"
+            print(error_message)
+            messages.error(request, error_message)
+            return render(request, 'registro.html')
+
+    return render(request, 'registro.html')
+
